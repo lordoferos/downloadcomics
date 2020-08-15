1f1591479dc0d55e603eff84a20edc6d6b3efc4b
lordoferos <33201932+lordoferos@users.noreply.github.com>
2020-08-15 19:43:58 +0300

    Update mouseNow.py

--- mouseNow.py
+++ mouseNow.py
@@ -10,6 +10,8 @@ try:
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
         print(positionStr)
+        
+       # What to print when you leave the loop 
 except KeyboardInterrupt:
     print('\nDone.')
     print('\b' * len(positionStr), end='', flush=True)
