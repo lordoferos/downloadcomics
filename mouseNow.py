7a82f956f628e3a95aee34bd4af939a859f6cc1c
lordoferos <33201932+lordoferos@users.noreply.github.com>
2020-08-15 19:42:25 +0300

    check position of mouse

---
+++ mouseNow.py
@@ -0,0 +1,15 @@
+#! python3
+# mouseNow.py - Displays the mouse cursor's current position.
+
+import pyautogui
+print('Press Ctrl-C to quit.')
+
+try:
+    while True:
+        # Get and print mouse coordinates
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr)
+except KeyboardInterrupt:
+    print('\nDone.')
+    print('\b' * len(positionStr), end='', flush=True)
